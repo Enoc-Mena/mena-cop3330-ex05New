5e3d8ca17f0db331df3bbf07d8773370dfbdd0fa
Enoc Mena <enocschool@Enocs-iMac.local>
2021-07-07 14:20:06 -0400

    Initial commit

---
+++ src/main/java/com/ex05/Ex05.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Enoc Mena
+ */
+
+package com.ex05;
+import java.util.Scanner;
+
+public class Ex05 {
+
+    public static void main(String[] args) {
+
+        String firstNum, secondNum;
+
+        //Gets num 1
+        Scanner numOne = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        firstNum = numOne.nextLine();
+        int parseNumOne = Integer.parseInt(firstNum);
+
+        //Gets num 2
+        Scanner numTwo = new Scanner(System.in);
+        System.out.print("Enter second number: ");
+        secondNum = numTwo.nextLine();
+        int parseNumTwo = Integer.parseInt(secondNum);
+
+        //Math output
+        System.out.print(parseNumOne + " + " + parseNumTwo + " = " + (parseNumOne+parseNumTwo));
+        System.out.println("");
+        System.out.print(parseNumOne + " - " + parseNumTwo + " = " + (parseNumOne-parseNumTwo));
+        System.out.println("");
+        System.out.print(parseNumOne + " * " + parseNumTwo + " = " + (parseNumOne*parseNumTwo));
+        System.out.println("");
+        System.out.print(parseNumOne + " / " + parseNumTwo + " = " + (parseNumOne/parseNumTwo));
+
+    }
+
+}
